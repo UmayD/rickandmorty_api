986b83d2d3e4fe09c7b70d20c2f421f0d242aa3b
UmayD <umayd96@gmail.com>
2022-11-28 17:23:44 +0300

    edit pagination - add page interval

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -7,7 +7,7 @@ function Characters() {
     const [character, setCharacter] = useState([]);
     const [characters, setCharacters] = useState([]);
     const [randNumber, setRandNumber] = useState(Math.floor(Math.random() * 826));
-    const [pageNumber, setPageNumber] = useState(0);
+    const [pageNumber, setPageNumber] = useState(1);
     
     const getCharacter = () => {
         axios.get("https://rickandmortyapi.com/api/character/" + randNumber)
@@ -16,7 +16,7 @@ function Characters() {
 
     const getCharacters = (pageNumber) => {
         axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`)
-            .then((res) => setCharacters(res.data.results));
+            .then((res) => setCharacters([...res.data.results]));
     }
 
     useEffect(() => {
@@ -26,7 +26,9 @@ function Characters() {
     useEffect(() => {
         getCharacters(pageNumber);
     }, [pageNumber])
-    
+
+
+    console.log(pageNumber);
     return (
         <div className="main_page">
             <h1>Character</h1>
@@ -41,23 +43,21 @@ function Characters() {
             <button onClick={() => setRandNumber(Math.floor(Math.random() * 826))}>Random Character</button>
 
             <div className="load_more_characters">
-                    <h1>Characters</h1>
+                <h1>Characters</h1>
 
-                    {characters.map(character => (
-                        <Character
-                            key={character.id}
-                            name={character.name}
-                            image={character.image}
-                            status={character.status}
-                            species={character.species}
-                        />
-                    ))}
+                {characters.map(character => (
+                    <Character
+                        key={character.id}
+                        name={character.name}
+                        image={character.image}
+                        status={character.status}
+                        species={character.species}
+                    />
+                ))}
 
-                    <button onClick={() => setPageNumber(pageNumber + 1)}>Load</button>
+                {pageNumber > 1 && <button onClick={() => setPageNumber(pageNumber - 1)}>Prev.</button>}
+                {pageNumber < 42 && <button onClick={() => setPageNumber(pageNumber + 1)}>Next</button>}
             </div>
-            
-
-            
 
         </div>
     );
